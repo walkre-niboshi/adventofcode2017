8168ba009ef8730d0837a4d949cd4f54e28b3e71
walkre <boo880432@gmail.com>
2017-12-21 10:20:52 +0900

    solved day19

---
+++ day19/problem1.nim
@@ -0,0 +1,47 @@
+import strutils, sequtils
+
+var diagram = newSeq[string]()
+while true:
+    try:
+        diagram.add(stdin.readLine)
+    except IOError:
+        break
+
+var
+    y = 0
+    x = diagram[0].find('|')
+    dir = 3
+    letters = ""
+let
+    dy = [0, -1, 0, 1]
+    dx = [1, 0, -1, 0]
+while true:
+    let
+        py = y
+        px = x
+    y = y+dy[dir]
+    x = x+dx[dir]
+    
+    if y < 0 or diagram.len <= y or x < 0 or diagram[0].len <= x or diagram[y][x] == ' ':
+        break
+
+    if diagram[y][x].isAlphaAscii:
+        letters.add(diagram[y][x])
+
+    if diagram[y][x] == '+':
+        for i in 0..3:
+            let
+                ny = y+dy[i]
+                nx = x+dx[i]
+            
+            if ny == py and nx == px:
+                continue
+            if ny < 0 or diagram.len <= ny or nx < 0 or diagram[0].len <= nx:
+                continue
+            if diagram[ny][nx] == ' ':
+                continue
+            
+            dir = i
+            break
+
+echo(letters)
